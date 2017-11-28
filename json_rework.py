423a745c1c48138182aeeceeb9210c5e7993db3c
Artur Curinga <arturmcuringa@gmail.com>
2017-11-28 19:09:46 -0300

    Changing JSON in aditivos2

--- json_rework.py
+++ json_rework.py
@@ -1,12 +1,13 @@
 import json
 
-data = json.load(open('aditivos.json'))
+data = json.load(open('json/contratos/aditivos.json'))
 l = data['_embbeded']
-with open('aditivos2.json', 'a') as f:
-	f.write('{"_embbeded":{"aditivos":[')
+with open('aditivos3.json', 'a') as f:
+	f.write('{"_embedded":{"aditivos":[')
 	for i,k in enumerate(l):
-		if i != 0:
-			f.write(",")
-		json.dump(k,f)
+		if '_embedded' in k:
+			json.dump(k['_embedded']['aditivos'],f)
+		else:
+			print(k)
 	f.write(']}}')
 		
