a1c95b3791a78ddc4a4bb6247a028bf581e7d386
Vitor Greati <greati@ufrn.edu.br>
2017-12-08 10:00:43 -0300

    Organize repos

--- sqls/portes_empresa_inserts.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO PorteEmpresa(id,descricao) VALUES ('1','MICROEMPRESA');
-INSERT INTO PorteEmpresa(id,descricao) VALUES ('3','EMPRESA DE PEQUENO PORTE');
-INSERT INTO PorteEmpresa(id,descricao) VALUES ('5','DEMAIS');
